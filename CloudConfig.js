a6fa90b38ba6a8d9b5cfa6a7dae661482699fc99
Angad-Singh16 <angad.singh.264356@gmail.com>
2025-08-26 13:16:32 +0530

    Update CloudConfig.js

--- CloudConfig.js
+++ CloudConfig.js
@@ -3,8 +3,8 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 
 cloudinary.config({
-    cloud_name: process.env.Cloud_Name,
-    api_key: process.env.Cloud_API_KEY,
+    cloud_name: process.env.CLOUD_Name,
+    api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET
 });
 
